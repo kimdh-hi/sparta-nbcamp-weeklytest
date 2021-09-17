ba1f63cb45820c767430e858a15a06765458d34a
kimdaehuyn <zbeld123@gmail.com>
2021-09-17 18:54:35 +0900

    제출

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbStock
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+
+@app.route('/codes')
+def codes():
+   group = request.args.get('group')
+   res = db.codes.find({'group':group})
+
+   print(res)
+
+   return jsonify({'result':codes})
+
+@app.route('/base/codes')
+def codes():
+   codes = db.codes.find({}, {'_id':False,'code':False,'name':False})
+   codes = codes.distinct('group')
+
+   return jsonify({'codes':codes})
+
+
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)

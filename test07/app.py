e3a01151f65a42e576817140910e69c80159a605
kimdh-hi <zbeld123@gmail.com>
2021-11-05 18:33:39 +0900

    댓글기능 완료

---
+++ test07/app.py
@@ -0,0 +1,200 @@
+import os
+from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request, Response
+from datetime import datetime, timedelta
+import jwt
+
+
+app = Flask(__name__)
+
+SECRET = "secret"
+
+client = MongoClient('localhost', 27017)
+db = client.test3
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/api/memo', methods=['POST'])
+def create():
+
+    token = request.headers.get("Authorization")
+    print(token)
+
+    id = ''
+    if token is not None:
+        try:
+            id = jwt.decode(token, SECRET, algorithms="HS256")['id']
+        except jwt.InvalidTokenError:
+            return Response(status=401)
+    if id != '':
+        writer_id = id
+    else:
+        writer_id = '비회원'
+
+    title = request.form['title']
+    comment = request.form['comment']
+    date = datetime.now().strftime('%Y.%m.%d %H:%M:%S')
+
+    total_count = db.memos.count()
+
+    if total_count > 0:
+        memo_id = db.memos.find_one(sort=[("memo_id", -1)])['memo_id'] + 1
+    else:
+        memo_id = 1
+
+    doc = {
+        "memo_id":memo_id,
+        "title":title,
+        "comment":comment,
+        "date":date,
+        "click":0,
+        'writer_id': writer_id
+    }
+
+    x = db.memos.insert_one(doc)
+
+    return jsonify({'msg': '저장완료'})
+
+@app.route('/api/memos', methods=['GET'])
+def read():
+    order = request.args.get('order')
+    limit = int(request.args.get('limit'))
+    page = int(request.args.get('page'))
+    type = request.args.get('type')
+
+    print(type)
+
+    total_count = db.memos.find({}).count()
+    total_page = (total_count // limit) + (1 if total_count % limit >= 1 else 0)
+    skip = (page-1) * limit
+
+    if type == 'my':
+        token = request.headers.get('Authorization')
+        try:
+            id = jwt.decode(token, SECRET, algorithms="HS256")['id']
+        except jwt.InvalidTokenError:
+            return Response(status=401)
+
+        if order == 'asc':
+            memos = list(db.memos.find({'writer_id': id}, {'_id': False}).sort([('click', 1)]).skip(skip).limit(limit))
+        else:
+            memos = list(db.memos.find({'writer_id': id}, {'_id': False}).sort([('click', -1)]).skip(skip).limit(limit))
+    else:
+        if order == 'asc':
+            memos = list(db.memos.find({}, {'_id': False}).sort([('click', 1)]).skip(skip).limit(limit))
+        else:
+            memos = list(db.memos.find({}, {'_id': False}).sort([('click', -1)]).skip(skip).limit(limit))
+
+    paging = {
+        'total_count': total_count,
+        'total_page': total_page,
+        'page': page,
+    }
+
+    return jsonify({"memos": memos, 'paging': paging})
+
+@app.route('/api/memo/<int:id>', methods=['GET'])
+def read_details(id):
+    click = db.memos.find_one({'memo_id': id}, {'_id': False})['click']
+    new_click = click+1
+
+    db.memos.update({'memo_id': id}, {'$set': {'click': new_click}})
+
+    memo = db.memos.find_one({'memo_id': id}, {'_id': 0})
+
+    comments = list(db.comments.find({'memo_id': id}, {'_id': False}))
+    print(comments)
+
+    return jsonify({"memo": memo, "comments": comments})
+
+@app.route('/api/memo', methods=['GET'])
+def read_details_edit_form():
+    memo_id = int(request.args.get('id'))
+
+    memo = db.memos.find_one({'memo_id': memo_id}, {'_id': 0})
+
+    return jsonify({"memo": memo})
+
+@app.route('/api/memo', methods=['PUT'])
+def update():
+    memo_id = int(request.form['id'])
+    title = request.form['title']
+    comment = request.form['comment']
+
+    print(memo_id, title, comment)
+
+    db.memos.update_one(
+        {'memo_id': memo_id},
+        {'$set': {'title': title, 'comment': comment}}
+    )
+
+    return jsonify({"msg": "수정완료"})
+
+@app.route('/api/memo', methods=['DELETE'])
+def delete():
+    id = int(request.args.get('id'))
+    db.memos.delete_one({'memo_id': id})
+    db.comments.delete_many({'memo_id': id})
+    return jsonify({'msg': '삭제완료'})
+
+
+#========= 회원가입 / 로그인 ============#
+
+@app.route('/signup', methods=['POST'])
+def signup():
+
+    print('회원가입')
+
+    id = request.form['id']
+    pw = request.form['pw']
+
+    print(id, pw)
+
+    find_user = db.members.find_one({'id': id})
+    if find_user is not None:
+        return jsonify({"result": 'fail', "msg":"이미 존재하는 ID 입니다."})
+    else:
+        doc = {
+            'id': id,
+            'password': pw
+        }
+        db.members.insert_one(doc)
+    return jsonify({"result": "success", "msg": "회원가입에 성공했습니다."})
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    id = request.form['id']
+    pw = request.form['pw']
+
+    find_user = db.members.find_one({'id': id, 'password': pw}, {'_id': False})
+
+    if find_user is None:
+        return jsonify({"result": 'fail', "msg": "인증에 실패했습니다."})
+
+    payload = {
+        "id": id,
+        "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 1)
+    }
+    token = jwt.encode(payload, SECRET, algorithm='HS256')
+
+    return jsonify({"result": "success", "msg": "로그인에 성공했습니다.", "token": token})
+
+@app.route('/comment', methods=['POST'])
+def add_comment():
+    comment = request.form['comment']
+    memo_id = int(request.form['memo_id'])
+
+    doc = {
+        'comment': comment,
+        'memo_id': memo_id
+    }
+
+    db.comments.insert_one(doc)
+
+    return jsonify({"result": "success", "msg": "댓글달기 완료"})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ test07/static/js/index.js
@@ -0,0 +1,307 @@
+order = "desc"
+limit = 5
+token = null
+type = "all"
+current_memo_id = null
+
+$(document).ready(function () {
+
+    if (sessionStorage.getItem('token') != null) {
+        token = sessionStorage.getItem('token')
+        $('#signin_btn').hide()
+        $('#signup_btn').hide()
+        $('#logout_btn').show()
+
+    } else {
+        $('#signin_btn').show()
+        $('#signup_btn').show()
+        $('#logout_btn').hide()
+    }
+
+    getMemos(1)
+
+    $("#select_page_limit").change(function () {
+        limit = $(this).val();
+        getMemos(1)
+    })
+
+    $.ajaxSetup({
+        error: function (jqXHR, exception) {
+            switch (jqXHR.status) {
+                case 401:
+                    alert('인증 에러!!');
+                    break;
+                case 423:
+                    alert('중복된 id!!');
+                    break;
+            }
+        },
+        beforeSend: function (xhr) {
+            if (sessionStorage.getItem('token') != null) {
+                xhr.setRequestHeader('Authorization', sessionStorage.getItem('token'));
+            }
+        }
+    });
+})
+
+function toggleButton() {
+    if ($("#post-box").css("display") == "none") {
+        $('#edit-button').hide()
+        $('#save-button').show()
+        $("#post-box").show()
+        $("#btn-post-box").text("포스트 박스 닫기")
+    } else {
+        $('#edit-button').hide()
+        $('#save-button').show()
+        $("#post-box").hide()
+        $('#post-title').val('');
+        $('#post-comment').val('');
+        $("#btn-post-box").text("포스트 박스 열기")
+    }
+}
+
+function postMemo() {
+    title = $("#post-title").val()
+    comment = $("#post-comment").val()
+
+    $.ajax({
+        type: "POST",
+        url: "/api/memo",
+        data: {"title": title, "comment": comment},
+        success: function (res) {
+            alert(res['msg'])
+            window.location.reload()
+        }
+    })
+}
+
+function getMemos(current_page) {
+    $('#memo-body').empty()
+    $.ajax({
+        type: "GET",
+        url: `/api/memos?order=${order}&page=${current_page}&limit=${limit}&type=${type}`,
+        success: function (res) {
+            memos = res['memos']
+            for (let i = 0; i < memos.length; i++) {
+                memo_number = (i + 1) + limit * (current_page - 1)
+                let tmp_html = `<tr>
+                                  <th scope="row">${memo_number}</th>
+                                  <td><a onclick="getMemo('${memos[i]['memo_id']}')">${memos[i]['title']}</a></td>
+                                  <td>${memos[i]['writer_id']}</td>
+                                  <td>${memos[i]['date']}</td>
+                                  <td>${memos[i]['click']}</td>
+                                  <td>
+                                    <button onclick="delete_memo('${memos[i]['memo_id']}')" type="button" class="btn btn-danger">삭제</button>
+                                  </td>
+                                  <td>
+                                    <button onclick="show_memo_edit_form('${memos[i]['memo_id']}')" type="button" class="btn btn-primary">수정</button>
+                                  </td>
+                                </tr>`
+                $('#memo-table').append(tmp_html)
+
+                makePagingButtons(res['paging'])
+            }
+
+        }
+    })
+}
+
+function makePagingButtons(paging_info) {
+    $('#pagination-buttons').empty()
+    total_page = paging_info['total_page']
+    current_page = paging_info['page']
+    tmp_html = ``
+    for (let i = 0; i < total_page; i++) {
+        if (current_page == i + 1) {
+            tmp_html = `<li class="page-item active"><a class="page-link" onclick="getMemos('${i + 1}')">${i + 1}</a></li>`
+        } else {
+            tmp_html = `<li class="page-item"><a class="page-link" onclick="getMemos('${i + 1}')">${i + 1}</a></li>`
+        }
+
+        $('#pagination-buttons').append(tmp_html)
+    }
+}
+
+function getMemo(memo_id) {
+
+    current_memo_id = memo_id
+
+    $.ajax({
+        type: "GET",
+        url: `/api/memo/${memo_id}`,
+        success: function (res) {
+            let title = res['memo']['title']
+            let comment = res['memo']['comment']
+            $('#modal-title').html(title);
+            $('#modal-content').html(comment);
+            $('#articleModal').modal('show');
+
+            let comments = res['comments']
+
+             $('#modal-comment').empty()
+            for (let i=0;i<comments.length;i++) {
+                let tmp_html = `<li class="list-group-item">${comments[i]['comment']}</li>`
+                $('#modal-comment').append(tmp_html)
+            }
+
+            getMemos(1)
+        }
+    })
+}
+
+function show_memo_edit_form(memo_id) {
+    $.ajax({
+        type: "GET",
+        url: `/api/memo?id=${memo_id}`,
+        success: function (res) {
+            let memo_id = res['memo']['memo_id']
+            let title = res['memo']['title']
+            let comment = res['memo']['comment']
+
+            current_memo_id = memo_id
+
+            $("#post-id").val(memo_id)
+            $("#post-title").val(title)
+            $('#post-comment').val(comment)
+            $('#btn-post-box').text("포스트 박스 닫기")
+
+            $('#edit-button').show()
+            $('#save-button').hide()
+
+            $('#post-box').show()
+        }
+    })
+}
+
+function editMemo() {
+    memo_id = $('#post-id').val()
+    title = $('#post-title').val()
+    comment = $('#post-comment').val()
+    $.ajax({
+        type: "PUT",
+        url: `/api/memo`,
+        data: {"id": memo_id, "title": title, "comment": comment},
+        success: function (res) {
+            alert(res['msg'])
+            window.location.reload()
+        }
+    })
+}
+
+function delete_memo(memo_id) {
+    $.ajax({
+        type: "DELETE",
+        url: `/api/memo?id=${memo_id}`,
+        success: function (res) {
+            alert(res['msg'])
+            window.location.reload();
+        }
+    })
+}
+
+function sort() {
+
+    order == "asc" ? console.log("오름차순 정렬") : console.log("내림차순정렬")
+
+    if (order == "asc") {
+        order = "desc"
+        $('#sort-icon').text("⬇️")
+    } else {
+        order = "asc"
+        $('#sort-icon').text("⬆️")
+    }
+
+    getMemos(1)
+}
+
+function open_signin_modal() {
+    $('#signinModal').modal('show');
+}
+
+function open_signup_modal() {
+    $('#signupModal').modal('show');
+}
+
+function signup() {
+    console.log('회원가입')
+    let signup_id = $('#signup-id').val()
+    let signup_pw = $('#signup-pw').val()
+    console.log(signup_id, signup_pw)
+    let doc = {
+        "id": signup_id,
+        "pw": signup_pw
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/signup",
+        data: doc,
+        success: function (res) {
+            alert(res.msg)
+            window.location.reload()
+        }
+    })
+}
+
+function signin() {
+    console.log('로그인')
+    let signin_id = $('#signin-id').val()
+    let signin_pw = $('#signin-pw').val()
+    let doc = {
+        "id": signin_id,
+        "pw": signin_pw
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/signin",
+        data: doc,
+        success: function (res) {
+            alert(res.msg)
+            if (res['result'] == 'success') {
+                sessionStorage.setItem('token', res['token'])
+                window.location.reload()
+            } else {
+                alert(res['msg'])
+            }
+
+        }
+    })
+}
+
+function logout() {
+    sessionStorage.clear()
+    window.location.reload()
+}
+
+function set_type(type_param) {
+    if (type_param == "my" && token == null) return false
+
+    if (type_param == type) return false
+
+    type = type_param
+    getMemos(1)
+}
+
+function saveComment() {
+
+    let comment = $('#comment').val()
+
+    console.log(comment)
+
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        data: {comment: comment, memo_id: current_memo_id},
+        success: function (res) {
+            if (res.result == "success") {
+                alert(res.msg)
+                $('#comment').val(' ')
+                let tmp_html = `<li class="list-group-item">${comment}</li>`
+                $('#modal-comment').append(tmp_html)
+            }
+        }
+    })
+}
+
+
